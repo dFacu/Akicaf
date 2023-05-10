e06f4e5b790d97076c7f018ccc58622d9ce6f67a
dFacu <d.facuangio@gmail.com>
2023-05-10 19:30:48 -0300

    2

--- Assets/Scrpits/PcManager.cs
+++ Assets/Scrpits/PcManager.cs
@@ -5,9 +5,37 @@ using UnityEngine;
 public class PcManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] productSpawn;
+    [SerializeField] private Transform purchasedProductPoint;
+    [SerializeField] private bool iBoughtSomeProduct = false;
+    [SerializeField] private float waitTime = 5f;
+    [SerializeField] private float inicioTime = 0f;
 
-    private void Array()
+
+    private void Update()
     {
+        waitTime += Time.deltaTime;
+
+        // se compro un producto entonce esperas
+        if (iBoughtSomeProduct == false)
+        {
+            buyProduct();
+        }
+
+        // tiempo de espera para comprar 
+        if(inicioTime > waitTime && iBoughtSomeProduct == true)
+        {
+            iBoughtSomeProduct = false;
+        }
 
     }
+    
+    // compra del producto
+    void buyProduct()
+    {
+
+        iBoughtSomeProduct = true;
+        waitTime += Time.deltaTime;
+
+    }
+
 }
