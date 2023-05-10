37e54b1548de738d188a58d7b138fcca9fd41104
dFacu <d.facuangio@gmail.com>
2023-05-10 18:39:31 -0300

    1

---
+++ Assets/Scrpits/PcManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PcManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] productSpawn;
+
+    private void Array()
+    {
+
+    }
+}
